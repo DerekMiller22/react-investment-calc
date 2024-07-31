a219dcf372cc47ab34e4c99bc937ea3cb7183f36
dmillions <derek.m360@gmail.com>
2024-07-31 11:50:24 -0400

    results table with calculations dispay below calculations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import Header from "./components/header";
 import UserInput from "./components/userInput";
+import Results from "./components/results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    intialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputId, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputId]: +newValue,
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChangeInput={handleChange} userInput={userInput} />
+      <Results userInput={userInput} />
     </>
   );
 }
